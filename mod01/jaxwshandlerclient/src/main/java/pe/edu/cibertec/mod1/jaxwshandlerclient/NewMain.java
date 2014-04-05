f05beaa506cef503eca0cdff9c01f779e914b0ab
user <user@user-VirtualBox>
2014-04-05 08:32:14 -0500

    Se genera el codigo de prueba

--- mod01/jaxwshandlerclient/src/main/java/pe/edu/cibertec/mod1/jaxwshandlerclient/NewMain.java
+++ mod01/jaxwshandlerclient/src/main/java/pe/edu/cibertec/mod1/jaxwshandlerclient/NewMain.java
@@ -3,8 +3,10 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package pe.edu.cibertec.mod1.jaxwshandlerclient;
+
+import javax.xml.ws.WebServiceRef;
+import pe.edu.cibertec.mod1.jaxwshandler.Entrenamiento_Service;
 
 /**
  *
@@ -16,7 +18,20 @@ public class NewMain {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+
+        try { // Call Web Service Operation
+            Entrenamiento_Service service = new Entrenamiento_Service();
+            pe.edu.cibertec.mod1.jaxwshandler.Entrenamiento port = service.getEntrenamientoPort();
+            // TODO initialize WS operation arguments here
+            java.lang.String latitud = "";
+            java.lang.String longitud = "";
+            // TODO process result here
+            java.util.List<pe.edu.cibertec.mod1.jaxwshandler.Nivel> result = port.obtenerRutas(latitud, longitud);
+            System.out.println("Result = " + result);
+        } catch (Exception ex) {
+            // TODO handle custom exceptions here
+        }
+
     }
-    
+
 }
